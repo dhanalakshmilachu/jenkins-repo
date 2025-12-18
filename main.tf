434684c5f2af7279272b5273ba19b29530993d1c
dhanalakshmilachu <dhanalakshmilachu894@gmail.com>
2025-12-18 14:32:25 +0530

    seventh commit

--- main.tf
+++ main.tf
@@ -13,6 +13,24 @@ resource "aws_instance" "jenkins_ec2" {
     Name = "Jenkins-Terraform-EC2"
   }
 }
+resource "aws_security_group" "sg_http" {
+  name        = "allow_http"
+  description = "Allow HTTP inbound"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 output "ec2_public_ip" {
   value = aws_instance.jenkins_ec2.public_ip
