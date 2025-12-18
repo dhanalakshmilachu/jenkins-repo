b2432f649db184494b3f45c916ae1b5977ea814b
dhanalakshmilachu <dhanalakshmilachu894@gmail.com>
2025-12-18 14:39:54 +0530

    seventh commit

--- main.tf
+++ main.tf
@@ -2,26 +2,25 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-resource "aws_instance" "jenkins_ec2" {
-  ami           = "ami-0fa91bc90632c73c9"   # Ubuntu AMI (Stockholm)
-  instance_type = "t3.micro"
-  key_name      = "new-key"                  # existing keypair name
-
-  user_data = file("${path.module}/userdata.sh")
-
-  tags = {
-    Name = "Jenkins-Terraform-EC2"
-  }
-}
-resource "aws_security_group" "sg_http" {
-  name        = "allow_http"
-  description = "Allow HTTP inbound"
+# Security Group to allow SSH and HTTP from anywhere
+resource "aws_security_group" "jenkins_sg" {
+  name        = "jenkins-sg"
+  description = "Allow SSH and HTTP from anywhere"
 
   ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # SSH open to all IPs (less secure)
+  }
+
+  ingress {
+    description = "HTTP"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]  # HTTP open to all IPs
   }
 
   egress {
@@ -32,7 +31,27 @@ resource "aws_security_group" "sg_http" {
   }
 }
 
+# Key pair (assumes you already created one in AWS)
+resource "aws_key_pair" "jenkins_key" {
+  key_name   = "new-key"                     # Replace with your key name
+  public_key = file("~/.ssh/new-key.pub")    # Replace with your public key path
+}
+
+# EC2 Instance
+resource "aws_instance" "jenkins_ec2" {
+  ami             = "ami-0fa91bc90632c73c9"  # Ubuntu 22.04 LTS in eu-north-1
+  instance_type   = "t3.micro"
+  key_name        = aws_key_pair.jenkins_key.key_name
+  security_groups = [aws_security_group.jenkins_sg.name]
+
+  tags = {
+    Name = "Jenkins-Terraform-EC2"
+  }
+}
+
+# Output the public IP
 output "ec2_public_ip" {
   value = aws_instance.jenkins_ec2.public_ip
 }
+
 
